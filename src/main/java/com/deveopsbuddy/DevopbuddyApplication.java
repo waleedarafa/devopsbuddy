f663736a9fa56cc4f40a5613256ce6bd3f696486
waleedarafa <engwaleed.arafa@gmail.com>
2017-04-15 17:05:41 +0200

    initial project commit

---
+++ src/main/java/com/deveopsbuddy/DevopbuddyApplication.java
@@ -0,0 +1,12 @@
+package com.deveopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopbuddyApplication.class, args);
+	}
+}
